a4bbe8a637f2ac2d23259b9c62ca8643a07dc442
Gabriel Feitosa Rato <gabriel.rato@usp.br>
2020-11-28 16:42:09 -0300

    Update uri2300.java

--- uri2300.java
+++ uri2300.java
@@ -1,65 +1,99 @@
 package com.gabriel;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 
 public class Main {
 
+    public static String getMinOpen(Map<String, Integer> shortestPath, Set<String> alreadyClosed) {
+        return shortestPath.entrySet().stream().filter(node -> !alreadyClosed.contains(node.getKey())).min(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+    }
+
+    private static void dijkstra(Graph graph, Node source) {
+        source.initiateDist(graph.nodes);
+        source.shortestPath.put(source.name, 0);
+        Set<String> alreadyClosed = new HashSet<>();
+
+        int i = 0;
+        while (i < graph.nodes.size()) {
+            String name = getMinOpen(source.shortestPath, alreadyClosed);
+
+            Node currNode = graph.getNode(name);
+            if (currNode == null) {
+                i++;
+                alreadyClosed.add(name);
+                continue;
+            }
+
+            for (Map.Entry<String, Integer> adjacencyPair : currNode.getAdjacentNodes().entrySet()) {
+                if (source.shortestPath.get(currNode.name) != Integer.MAX_VALUE && adjacencyPair.getValue() + source.shortestPath.get(currNode.name) < source.shortestPath.getOrDefault(adjacencyPair.getKey(), Integer.MAX_VALUE)) {
+                    source.shortestPath.put(adjacencyPair.getKey(), adjacencyPair.getValue() + source.shortestPath.get(currNode.name));
+                }
+            }
+            alreadyClosed.add(currNode.name);
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int testCase = 0;
         Integer n = reader.nextInt();
         Integer e = reader.nextInt();
-        while (e != 0) {
-            testCase++;
-            Graph graph = new Graph();
+        while (n != 0) {
 
+            Graph graph = new Graph();
             for (int i = 0; i < e; i++) {
                 String source = String.valueOf(reader.nextInt());
                 String destination = String.valueOf(reader.nextInt());
-                Node nodeSource;
-                Node nodeDestination;
-                if (graph.getNode(source) != null) {
-                    nodeSource = graph.getNode(source);
+                Node node = graph.getNode(source);
+                if (node == null) {
+                    node = new Node(source);
+                }
+
+                int idx = graph.sameCountry(destination, node.name);
+                if (idx != -1) {
+                    Node possible = graph.nodes.get(idx);
+                    reader.nextInt();
+                    node.addDestination(destination, 0);
+                    possible.addDestination(source, 0);
+
                 } else {
-                    nodeSource = new Node(source);
+                    node.addDestination(destination, reader.nextInt());
                 }
 
-                if (graph.nameToIdx.get(destination) != null) {
-                    nodeDestination = graph.getNode(destination);
-                } else {
-                    nodeDestination = new Node(destination);
-                }
-
-                // nao importa o peso
-                nodeSource.addDestination(destination, 1);
-                nodeDestination.addDestination(source, 1);
-
-                if (graph.nameToIdx.get(source) == null) {
-                    graph.addNode(nodeSource);
-                }
-                if (graph.nameToIdx.get(destination) == null) {
-                    graph.addNode(nodeDestination);
-                }
+                graph.addNodeIfNew(node);
             }
 
-            if (n > graph.nodes.size()) {
-                System.out.println("Teste " + testCase);
-                System.out.println("falha");
-                System.out.println();
-            } else {
-                System.out.println("Teste " + testCase);
-                System.out.println(graph.isConnected(n) ? "normal" : "falha");
-                System.out.println();
+            int k = reader.nextInt();
+            Set<String> computedAlready = new HashSet<>();
+            for (int i = 0; i < k; i++) {
+                String source = String.valueOf(reader.nextInt());
+                String destination = String.valueOf(reader.nextInt());
+                if (graph.getNode(source) == null) {
+                    System.out.println("Nao e possivel entregar a carta");
+                } else {
+                    Node goPoint = graph.getNode(source);
+                    if(!computedAlready.contains(goPoint.name)) {
+                        dijkstra(graph, goPoint);
+                        computedAlready.add(goPoint.name);
+                    }
+                    System.out.println(goPoint.shortestPath.get(destination) != Integer.MAX_VALUE ? goPoint.shortestPath.get(destination) : "Nao e possivel entregar a carta");
+                }
             }
-
             n = reader.nextInt();
             if (n == 0) {
+                System.out.println();
                 break;
             }
             e = reader.nextInt();
-
+            System.out.println();
         }
 
     }
@@ -68,6 +102,7 @@ public class Main {
 
         Map<String, Integer> adjacentNodes = new HashMap<>();
         private String name;
+        private Map<String, Integer> shortestPath = new HashMap<>();
 
         public Node(String name) {
             this.name = name;
@@ -75,6 +110,12 @@ public class Main {
 
         public void addDestination(String destination, int distance) {
             adjacentNodes.put(destination, distance);
+        }
+
+        // tenho q saber todos Nos daquele grafo...
+        public Map<String, Integer> initiateDist(List<Node> nodes) {
+            nodes.forEach(node -> shortestPath.put(node.name, Integer.MAX_VALUE));
+            return this.shortestPath;
         }
 
         public Map<String, Integer> getAdjacentNodes() {
@@ -87,41 +128,29 @@ public class Main {
         private List<Node> nodes = new ArrayList<>();
         private Map<String, Integer> nameToIdx = new HashMap<>();
 
-        public boolean isConnected(int qtdNodes) {
-            AtomicInteger qtd = new AtomicInteger();
-            nodes.forEach(node -> qtd.addAndGet(canReachAllNodes(node, qtdNodes) ? 1 : 0));
-            return qtd.intValue() == nodes.size();
-        }
-
         public Node getNode(String source) {
             if (nameToIdx.get(source) == null) return null;
             return nodes.get(nameToIdx.get(source));
         }
 
-        public boolean canReachAllNodes(Node source, int qtdNodes) {
-            Queue<Node> nodeQueue = new ArrayDeque<>();
-            Set<String> visited = new HashSet<>();
-
-            visited.add(source.name);
-            nodeQueue.add(source);
-
-            while (!nodeQueue.isEmpty()) {
-                Node currNode = nodeQueue.remove();
-                for (Map.Entry<String, Integer> adjacencyPair : currNode.getAdjacentNodes().entrySet()) {
-                    if (!visited.contains(adjacencyPair.getKey())) {
-                        visited.add(adjacencyPair.getKey());
-                        nodeQueue.add(nodes.get(nameToIdx.get(adjacencyPair.getKey())));
+        public int sameCountry(String destination, String source) {
+            for (int i = 0; i < nodes.size(); i++) {
+                if (nodes.get(i).name.equals(destination)) {
+                    if (nodes.get(i).adjacentNodes.get(source) != null) {
+                        return i;
                     }
                 }
+
             }
-            return qtdNodes == visited.size();
+            return -1;
         }
 
-        public void addNode(Node nodeA) {
-            if (nameToIdx.get(nodeA.name) == null)
+        public void addNodeIfNew(Node nodeA) {
+            if (nameToIdx.get(nodeA.name) == null) {
                 nameToIdx.put(nodeA.name, nodes.size());
-            nodes.add(nodeA);
+                nodes.add(nodeA);
+            }
         }
-
     }
 }
+
